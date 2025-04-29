bb3b526d580e4333d546fc6ede54964e7714e231
Thomas <thomas@mac.home>
2025-04-29 21:34:52 +0200

    stripe redirect

--- src/pages/redirect/stripe/index.tsx
+++ src/pages/redirect/stripe/index.tsx
@@ -1,9 +1,11 @@
+import { Spinner } from '@/components/Spinner'
 import { useAuthSession } from '@/hooks/nextAuth.hook'
 import { trpc } from '@/utils/trpc'
-import { Container, Box } from '@mui/material'
+import { Container, Box, Typography } from '@mui/material'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { useRouter } from 'next/router'
-import { ReactElement, useEffect } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import { z } from 'zod'
 
 const Base = ({ children }: { children: ReactElement }) => {
@@ -27,6 +29,8 @@ const qsValidator = z.object({
 
 const Proceed = ({ query }: { query: z.infer<typeof qsValidator> }) => {
   const { mutateAsync } = trpc.protectedMutation.offer.accept.useMutation()
+  const [showSpinner, setShowSpinner] = useState(true)
+  const [error, setError] = useState<string>()
 
   useEffect(() => {
     mutateAsync({
@@ -34,7 +38,50 @@ const Proceed = ({ query }: { query: z.infer<typeof qsValidator> }) => {
       paymentProvider: 'stripe',
       paymentId: query.paymentId
     })
+      .catch(error => {
+        console.warn('Error accepting offer:', error)
+        setError(error.message)
+      })
+      .finally(() => {
+        setShowSpinner(false)
+      })
   }, [query])
+
+  if (showSpinner) {
+    return (
+      <Base>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh'
+          }}
+        >
+          <Spinner />
+        </Box>
+      </Base>
+    )
+  }
+
+  if (error) {
+    return (
+      <Base>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh'
+          }}
+        >
+          <Typography variant="h4" color="error" component="p">
+            {error}
+          </Typography>
+        </Box>
+      </Base>
+    )
+  }
 
   return (
     <Base>
@@ -46,7 +93,7 @@ const Proceed = ({ query }: { query: z.infer<typeof qsValidator> }) => {
           height: '100vh'
         }}
       >
-        <h1>Payment successful</h1>
+        <Link href={`/dashboard/detail/offer/${query.offerId}`}>Go bask to offer</Link>
       </Box>
     </Base>
   )
